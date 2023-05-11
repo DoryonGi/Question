397c587990b1fddcf45ce7e8db6dbbafcd67ca3e
끼얏호우끼얏호우 <jiwooson.0816@gmail.com>
2023-05-11 09:54:21 +0900

    Initial Commit

---
+++ Question/QuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp.swift
+//  Question
+//
+//  Created by sonjiwoo on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
