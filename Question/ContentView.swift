f41a02a007f01c497968da538e646b1d8ff2baee
끼얏호우끼얏호우 <jiwooson.0816@gmail.com>
2023-05-12 10:11:08 +0900

    작은 글씨

--- Question/ContentView.swift
+++ Question/ContentView.swift
@@ -15,20 +15,31 @@ struct ContentView: View {
                 ZStack{
                     Circle()
                         .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                    
                     Text("1")
                         .font(.system(size: 37))
+                    
                 }
                 ZStack{
                     Circle()
                         .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
-                    Text("2")
-                        .font(.system(size: 37))
+                    VStack{
+                        Text("2")
+                            .font(.system(size: 37))
+                        Text("ABC")
+                            .font(.system(size: 10))
+                           
+                    }
                 }
                 ZStack{
                     Circle()
                         .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
-                    Text("3")
-                        .font(.system(size: 37))
+                    VStack{
+                        Text("3")
+                            .font(.system(size: 37))
+                        Text("DEF")
+                            .font(.system(size: 10))
+                    }
                 }
             }
             VStack(spacing: 18){
@@ -36,23 +47,35 @@ struct ContentView: View {
                     ZStack{
                         Circle()
                             .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
-                        Text("4")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("4")
+                                .font(.system(size: 37))
+                            Text("GHI")
+                                .font(.system(size: 10))
+                        }
                         
                     }
                     ZStack{
                         Circle()
                             .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
-                        Text("5")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("5")
+                                .font(.system(size: 37))
+                            Text("JKL")
+                                .font(.system(size: 10))
+                        }
                         
                         
                     }
                     ZStack{
                         Circle()
                             .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
-                        Text("6")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("6")
+                                .font(.system(size: 37))
+                            Text("MNO")
+                                .font(.system(size: 10))
+                        }
                     }
                 }
             }
@@ -62,23 +85,35 @@ struct ContentView: View {
                         Circle()
                             .frame(width:78 ,height:78)
                             .foregroundColor(Color(UIColor.systemGray5))
-                        Text("7")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("7")
+                                .font(.system(size: 37))
+                            Text("PQRS")
+                                .font(.system(size: 10))
+                        }
                         
                     }
                     ZStack{
                         Circle()
                             .frame(width:78 ,height:78)
                             .foregroundColor(Color(UIColor.systemGray5))
-                        Text("8")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("8")
+                                .font(.system(size: 37))
+                            Text("TUV")
+                                .font(.system(size: 10))
+                        }
                     }
                     ZStack{
                         Circle()
                             .frame(width:78 ,height:78)
                             .foregroundColor(Color(UIColor.systemGray5))
-                        Text("9")
-                            .font(.system(size: 37))
+                        VStack{
+                            Text("9")
+                                .font(.system(size: 37))
+                            Text("WXYZ")
+                                .font(.system(size: 10))
+                        }
                         
                     }
                     
@@ -99,8 +134,13 @@ struct ContentView: View {
                             Circle()
                                 .frame(width:78 ,height:78)
                                 .foregroundColor(Color(UIColor.systemGray5))
-                            Text("0")
-                                .font(.system(size: 37))
+                            VStack{
+                                Text("0")
+                                    .font(.system(size: 37))
+                                Text("+")
+                                    .font(.system(size: 14
+                                                ))
+                            }
                         }
                         ZStack{
                             Circle()
@@ -124,6 +164,7 @@ struct ContentView: View {
                                 Image(systemName: "phone.fill")
                                     .resizable()
                                     .frame(width:38, height:42)
+                                    .foregroundColor(.white)
                                 
                                 
                                 
