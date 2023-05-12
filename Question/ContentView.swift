7c788a24793d91d3896e09cb5f4b211ac529b88a
끼얏호우끼얏호우 <jiwooson.0816@gmail.com>
2023-05-12 09:57:40 +0900

    전화기

--- Question/ContentView.swift
+++ Question/ContentView.swift
@@ -9,18 +9,148 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 18){
+            HStack(spacing: 24){
+                ZStack{
+                    Circle()
+                        .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                    Text("1")
+                        .font(.system(size: 37))
+                }
+                ZStack{
+                    Circle()
+                        .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                    Text("2")
+                        .font(.system(size: 37))
+                }
+                ZStack{
+                    Circle()
+                        .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                    Text("3")
+                        .font(.system(size: 37))
+                }
+            }
+            VStack(spacing: 18){
+                HStack(spacing: 24){
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                        Text("4")
+                            .font(.system(size: 37))
+                        
+                    }
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                        Text("5")
+                            .font(.system(size: 37))
+                        
+                        
+                    }
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78).foregroundColor(Color(UIColor.systemGray5))
+                        Text("6")
+                            .font(.system(size: 37))
+                    }
+                }
+            }
+            VStack(spacing: 18){
+                HStack(spacing: 24){
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78)
+                            .foregroundColor(Color(UIColor.systemGray5))
+                        Text("7")
+                            .font(.system(size: 37))
+                        
+                    }
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78)
+                            .foregroundColor(Color(UIColor.systemGray5))
+                        Text("8")
+                            .font(.system(size: 37))
+                    }
+                    ZStack{
+                        Circle()
+                            .frame(width:78 ,height:78)
+                            .foregroundColor(Color(UIColor.systemGray5))
+                        Text("9")
+                            .font(.system(size: 37))
+                        
+                    }
+                    
+                    
+                }
+                VStack(spacing: 19.5){
+                    HStack(spacing: 24){
+                        ZStack{
+                            Circle()
+                                .frame(width:78 ,height:78)
+                                .foregroundColor(Color(UIColor.systemGray5))
+                            Text("﹡")
+                                .font(.system(size: 50
+                                             ))
+                            
+                        }
+                        ZStack{
+                            Circle()
+                                .frame(width:78 ,height:78)
+                                .foregroundColor(Color(UIColor.systemGray5))
+                            Text("0")
+                                .font(.system(size: 37))
+                        }
+                        ZStack{
+                            Circle()
+                                .frame(width:78 ,height:78)
+                                .foregroundColor(Color(UIColor.systemGray5))
+                            Text("#")
+                                .font(.system(size: 36))
+                            
+                        }
+                        
+                    }
+                    
+                    
+                    
+                    VStack{
+                       
+                            ZStack{
+                                Circle()
+                                    .frame(width:75,height:75)
+                                    .foregroundColor(Color.green)
+                                Image(systemName: "phone.fill")
+                                    .resizable()
+                                    .frame(width:38, height:42)
+                                
+                                
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            .padding()
         }
-        .padding()
+        
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
     }
-}
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
